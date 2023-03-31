575d76f24bd176375b5ee973d21cb2fb4431d620
itashdv <itashcb@gmail.com>
2023-03-31 16:57:09 +0300

    Initialize a project with Webpack. Add html plugin

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+/* eslint-disable */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'source-map',
+  output: {
+    filename: 'bundle.[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Pokedex',
+      filename: 'index.html',
+      template: 'src/template.html',
+    }),
+  ],
+};
